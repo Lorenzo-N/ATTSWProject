243386f335e1e73afb2b28769213255122bfa03d
Lorenzo Nuti <lorenzo.nuti@genomedics.it>
2020-06-09 19:34:27 +0200

    BDD

--- src/test/java/com/nuti/puccia/bdd/examReservationSteps.java
+++ src/test/java/com/nuti/puccia/bdd/examReservationSteps.java
@@ -1,5 +1,7 @@
 package com.nuti.puccia.bdd;
 
+import com.nuti.puccia.model.Exam;
+import com.nuti.puccia.model.Student;
 import org.assertj.swing.core.BasicRobot;
 import org.assertj.swing.core.GenericTypeMatcher;
 import org.assertj.swing.finder.WindowFinder;
@@ -9,7 +11,13 @@ import org.jbehave.core.model.ExamplesTable;
 import org.jbehave.core.steps.Parameters;
 import org.junit.After;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import javax.swing.*;
+
+import java.util.*;
+import java.util.regex.Pattern;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.swing.launcher.ApplicationLauncher.application;
@@ -17,97 +25,75 @@ import static org.junit.Assert.assertEquals;
 
 public class examReservationSteps {
 
-
+    private EntityManagerFactory entityManagerFactory;
+    private EntityManager entityManager;
     private FrameFixture window;
+
+    private final String name1 = "Andrea";
+    private final String surname1 = "Puccia";
+    private final String name2 = "Lorenzo";
+    private final String surname2 = "Nuti";
+    private final String name3 = "Mario";
+    private final String surname3 = "Rossi";
+    private final String exam1 = "ATTSW";
+    private final String exam2 = "Analisi";
+    private final String exam3 = "Fisica";
+
+    @BeforeStories
+    public void setUpStories() {
+        Map<String, String> settings = new HashMap<>();
+        settings.put("javax.persistence.jdbc.url", "jdbc:mysql://localhost:3306/attsw");
+        settings.put("javax.persistence.jdbc.user", "root");
+        settings.put("javax.persistence.jdbc.password", "");
+        entityManagerFactory = Persistence.createEntityManagerFactory("APP", settings);
+    }
+
+    @AfterStories
+    public void tearDownStories() {
+        entityManagerFactory.close();
+    }
+
+    @BeforeScenario
+    public void setUp() {
+        System.out.println("Prima di ogni scenario!");
+        entityManager = entityManagerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.createQuery("DELETE FROM Exam e").executeUpdate();
+        entityManager.createQuery("DELETE FROM Student s").executeUpdate();
+        entityManager.getTransaction().commit();
+    }
 
     @AfterScenario
     public void tearDown() {
-        if (window != null)
+        if (window != null) {
+//            window.close();
             window.cleanUp();
+        }
+        entityManager.close();
     }
 
-    @Given("The ExamReservation View is shown")
-    @When("The ExamReservation View is shown")
-    public void the_Student_View_is_shown() {
-        // start the Swing application
+    private void addStudentToDataBase(Student student) {
+        entityManager.getTransaction().begin();
+        entityManager.persist(student);
+        entityManager.getTransaction().commit();
+    }
+
+    private void addExamToDataBase(Exam exam) {
+        entityManager.getTransaction().begin();
+        entityManager.persist(exam);
+        entityManager.getTransaction().commit();
+    }
+
+    @Given("The Exam Reservation View is shown")
+    public void givenExamReservationViewIsShown() {
+        System.out.println("start!");
         application("com.nuti.puccia.App").start();
-        // get a reference of its JFrame
         window = WindowFinder.findFrame(new GenericTypeMatcher<JFrame>(JFrame.class) {
             @Override
             protected boolean isMatching(JFrame frame) {
                 return "Exam Reservations".equals(frame.getTitle()) && frame.isShowing();
             }
         }).using(BasicRobot.robotWithCurrentAwtHierarchy());
-//        window.iconify().deiconify().focus();
-//        window.iconify().deiconify();
-//        window.textBox("ExamNameText").enterText("");
-    }
-
-    @BeforeScenario
-    public void beforeEachScenario() {
-        System.out.println("Prima di ogni scenario!");
-    }
-
-//    @Given("a calculator with value <value>")
-//    public void givenACalculatorWithValueValue(@Named("value") int value) {
-//        c = new Calculator(value);
-//    }
-//
-//    @When("add the number <number>")
-//    public void whenAddTheNumberNumber(@Named("number") int number) {
-//        c.add(number);
-//    }
-//
-//    @Then("the result is <result>")
-//    public void thenTheResultIsResult(@Named("result") int result) {
-//        assertEquals(c.getResult(), result);
-//    }
-//
-//    @When("minus the number <number>")
-//    public void whenMinusTheNumberNumber(@Named("number") int number) {
-//        c.minus(number);
-//    }
-//
-//    @Given("a calculator with value $integer1")
-//    public void givenACalculatorWithValue(int integer1) {
-//        c = new Calculator(integer1);
-//    }
-//
-//    @When("add the number $integer1")
-//    public void whenAddTheNumber(int integer1) {
-//        c.add(integer1);
-//    }
-//
-//    @Then("the result is $integer1")
-//    public void thenTheResultIs(int integer1) {
-//        assertEquals(c.getResult(), integer1);
-//    }
-//
-//    @Given("a calculator with this values:$examplesTable")
-//    public void givenACalculatorWithThisValues(ExamplesTable examplesTable) {
-//        System.out.println("ciaoooooo");
-//        for (Parameters row : examplesTable.getRowsAsParameters()) {
-//            int a = row.valueAs("a", int.class);
-//            int b = row.valueAs("b", int.class);
-//            float c = row.valueAs("c", float.class);
-//            boolean d = row.valueAs("d", boolean.class);
-//            System.out.println((a+b));
-//            System.out.println((c+1.2));
-//            if(d){
-//                System.out.println("Success!");
-//            }
-//        }
-//    }
-
-    @Given("The database contains the students with the following values$examplesTable")
-    public void givenTheDatabaseContainsTheStudentsWithTheFollowingValues(ExamplesTable examplesTable) {
-
-    }
-
-    @Then("The student list contains elements with the following values$examplesTable")
-    public void thenTheStudentListContainsElementsWithTheFollowingValues(ExamplesTable examplesTable) {
-
-
     }
 
     @When("User write")
@@ -119,4 +105,57 @@ public class examReservationSteps {
     public void thenIsWritten() {
         assertThat(window.textBox("ExamNameText").text()).isEqualTo("ciaooo");
     }
+
+    @Given("The Database contains few students and few exams")
+    public void givenDatabaseContainsFewStudentsAndFewExams() {
+        System.out.println("background!");
+        Student student1 = new Student(name1, surname1);
+        Student student2 = new Student(name2, surname2);
+        addStudentToDataBase(student1);
+        addStudentToDataBase(student2);
+        addExamToDataBase(new Exam(exam1, new ArrayList<>(Collections.singletonList(student1))));
+        addExamToDataBase(new Exam(exam2, new ArrayList<>(Arrays.asList(student1, student2))));
+    }
+
+    @When("The user selects an Exam")
+    public void whenTheUserSelectsAnExam() {
+        window.list("ExamList").selectItem(Pattern.compile(".*" + exam1 + ".*"));
+    }
+
+    @Then("Exam list contains exams info")
+    public void thenExamListContainsExamsInfo() {
+        assertThat(window.list("ExamList").contents())
+                .anySatisfy(e -> assertThat(e).contains(exam1))
+                .anySatisfy(e -> assertThat(e).contains(exam2));
+    }
+
+    @Then("Student list contains students info")
+    public void thenStudentListContainsStudentsInfo() {
+        assertThat(window.list("StudentList").contents())
+                .anySatisfy(e -> assertThat(e).contains(name1, surname1))
+                .anySatisfy(e -> assertThat(e).contains(name2, surname2));
+    }
+
+    @Then("Reservation list contains reservations info")
+    public void thenReservationListContainsReservationsInfo() {
+        assertThat(window.list("ReservationList").contents()).hasSize(1)
+                .anySatisfy(e -> assertThat(e).contains(name1, surname1));
+    }
+
+    @Given("The user enter exam name")
+    public void givenTheUserEnterExamName() {
+        window.textBox("ExamNameText").enterText(exam3);
+    }
+
+    @When("The user clicks $buttonName button")
+    public void whenTheUserClicksAButton(String buttonName) {
+        window.button(buttonName).click();
+    }
+
+    @Then("Exam list contains new exam info")
+    public void thenExamListContainsNewExamInfo() {
+        assertThat(window.list("ExamList").contents())
+                .anySatisfy(e -> assertThat(e).contains(exam3));
+    }
+
 }
--- src/test/resources/stories/examReservation.story
+++ src/test/resources/stories/examReservation.story
@@ -1,19 +1,19 @@
 Narrative:
 Stories to test ExamReservations application
 
-!-- Scenario: The initial state of student list in the view
-!-- Given The database contains the students with the following values
-!-- |Name|Surname|
-!-- |Andrea|Puccia|
-!-- |Lorenzo|Nuti|
-!-- When The ExamReservation View is shown
-!-- Then The student list contains elements with the following values
-!-- |Text|
-!-- |Nuti Lorenzo (id:1)|
-!-- |Puccia Andrea (id:1)|
 
 
-Scenario: The initial state of student list in the view
-Given The ExamReservation View is shown
-When User write
-Then Is written
+Scenario: Initial state of student and exam list
+Given The Database contains few students and few exams
+And The Exam Reservation View is shown
+When The user selects an Exam
+Then Exam list contains exams info
+And Student list contains students info
+And Reservation list contains reservations info
+
+Scenario: Add exam
+Given The Database contains few students and few exams
+And The Exam Reservation View is shown
+And The user enter exam name
+When The user clicks AddExam button
+Then Exam list contains new exam info
